884ee3183f7dc0d0c94c7b84a4449ef139b7fd57
Douglinhas <daguiaralcantara@gmail.com>
2023-12-14 16:58:40 -0300

    feat: create ListProducts route

--- server/src/main/config/router.ts
+++ server/src/main/config/router.ts
@@ -1,9 +1,11 @@
 import { Router, Express } from "express";
 
 import createRouter from "../routes/create-product/create-product";
+import listProducts from "../routes/list-products/list-products";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/api/product", router);
   createRouter(router);
+  listProducts(router);
 }
