0f585816515a0f45444444dd34f85b0bdad7f6d7
Douglinhas <daguiaralcantara@gmail.com>
2023-12-16 15:36:27 -0300

    feat: create UpdateProduct controller

---
+++ server/src/presentation/controllers/update-product/update-product.ts
@@ -0,0 +1,38 @@
+import { Controller, UpdateProduct, ProductValidation, HttpRequest, HttpResponse, badRequest, MissingParamError, InvalidParamError, noContent, notFound, serverError } from "./update-product-protocols";
+
+export class UpdateProductController implements Controller {
+  constructor(
+    private updateProduct: UpdateProduct,
+    private productValidation: ProductValidation 
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const name = httpRequest.params?.name;
+    
+    if (!name) {
+      return badRequest(new MissingParamError("name"));
+    } else if (typeof name !== "string") {
+      return badRequest(new InvalidParamError("name", "Name must be a string"));
+    }
+
+    const validation = this.productValidation.handle(httpRequest.body);
+
+    if (!validation.isValid) {
+      return badRequest(validation.error!);
+    }
+
+    try {
+      const product = httpRequest.body;
+
+      await this.updateProduct.handle(name, product);
+      return noContent();
+    } catch (error) {
+      if (error instanceof Error && error.name === "NotFoundError") {
+        return notFound();
+      }
+
+      return serverError();
+    }
+  }
+
+}
