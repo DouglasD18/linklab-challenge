40e5c5c7bee10e09946c4a926469103cccb36114
Douglinhas <daguiaralcantara@gmail.com>
2023-12-17 12:36:58 -0300

    feat: create DeleteProduct controller

---
+++ server/src/presentation/controllers/delete-product/delete-product.ts
@@ -0,0 +1,28 @@
+import { Controller, DeleteProduct, HttpRequest, HttpResponse, badRequest, MissingParamError, InvalidParamError, noContent, notFound, serverError } from "./delete-product-protocols";
+
+export class DeleteProductController implements Controller {
+  constructor(private deleteProduct: DeleteProduct) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const name = httpRequest.params?.name;
+    
+    if (!name || name.length < 1) {
+      return badRequest(new MissingParamError("name"));
+    } else if (typeof name !== "string") {
+      return badRequest(new InvalidParamError("name", "Name must be a string"));
+    }
+    
+    try {
+      await this.deleteProduct.handle(name);
+
+      return noContent();
+    } catch (error) {
+      if (error instanceof Error && error.name === "NotFoundError") {
+        return notFound();
+      }
+
+      return serverError();
+    }
+  }
+  
+}
