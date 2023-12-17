9b96714969d84d75bc012c434cf011494c842b9d
Douglinhas <daguiaralcantara@gmail.com>
2023-12-17 12:58:37 -0300

    feat: create DeleteProduct adapter

---
+++ server/src/data/useCases/delete-product/delete-product.ts
@@ -0,0 +1,14 @@
+import { DeleteProduct, DeleteProductRepository, NotFoundError } from "./delete-product-protocols";
+
+export class DeleteProductAdapter implements DeleteProduct {
+  constructor(private repository: DeleteProductRepository) {}
+
+  async handle(name: string): Promise<void> {
+    const isDeleted = await this.repository.handle(name);
+
+    if (!isDeleted) {
+      throw new NotFoundError();
+    }
+  }
+
+}
