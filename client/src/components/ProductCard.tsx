8308eda0e27546f7e944fc4a1256104793d5dd4c
Douglinhas <daguiaralcantara@gmail.com>
2023-12-18 16:48:01 -0300

    feta: add some style

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -9,15 +9,15 @@ export function ProductCard({ product }: ProductCardProps) {
   const { name, image, note, value } = product;
 
   return (
-    <div>
-      <img src={ `data:image/svg+xml;utf8,${encodeURIComponent(image)}` } alt={ `${name} image` } />
-      <div>
+    <div className="product-card">
+      <img style={{ width: "18vw" }} src={ `data:image/svg+xml;utf8,${encodeURIComponent(image)}` } alt={ `${name} image` } />
+      <div className="infos">
         <div>
           <p>{ name }</p>
-          <p>Nota: { `${note.toFixed(1)}` }</p>
+          <p className="note">Nota: { `${note.toFixed(1)}` }</p>
         </div>
         <div>
-          <p>R$ { `${value.toFixed(2).toLocaleString()}` }</p>
+          <p>R$ { `${value.toFixed(2).replace(".", ",")}` }</p>
         </div>
       </div>
     </div>
