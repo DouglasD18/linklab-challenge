618fa3e7315649063c8138e14a585f0c3daacfdd
Douglinhas <daguiaralcantara@gmail.com>
2023-12-17 23:00:02 -0300

    refactore: change Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import Logo from "@/../public/images/logo.svg"
 import Painel from "@/../public/images/painel.svg"
+import Down from "@/../public/images/down.svg"
+import Top from "@/../public/images/top.svg"
+
 import { useState } from "react"
 import { HeaderProducts } from "./HeaderProducts";
 
@@ -17,9 +20,9 @@ export function Header() {
         <div className="header-navbar-content">
           <Image src={ Logo } alt={"Logo e nome da empresa"} />
           <div>Início</div>
-          <div onClick={ () => onClick() }>
-            Products
-            <span>{ showProducts ? " ^" : " ˇ"}</span>
+          <div onClick={ () => onClick() } >
+            Products {" "}
+            <span>{ showProducts ? <Image src={Top} alt={"Seta apontando para cima"} /> : <Image src={Down} alt={"Seta apontando para baixo"} />}</span>
           </div>
           <div>Laudos</div>
           <div>Sobre nós</div>
